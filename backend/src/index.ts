dcdce50f336e2b5e92b145e4bdcf88705f1c635d
Abdelrahman-Yaser <abdooy640@gmail.com>
2026-02-13 01:04:33 +0200

    fix: update CORS settings to allow production domain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3001;
 // ─────────────────────────────────────────
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173', 
+      'http://localhost:3000', 
+      'https://e-commmerce-mbc.netlify.app' //
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
